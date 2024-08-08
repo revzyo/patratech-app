aa3ac394bc28fb33b8dd36accc515a032355be83
revzyo <raghavdixitdixit@gmail.com>
2024-08-08 21:15:03 +0530

    commit

---
+++ firebase/firebase.ts
@@ -0,0 +1,7 @@
+import { initializeApp, getApp, getApps} from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    
+}
